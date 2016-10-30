02d9364258fb592bf95597e9ae50c527b522c4ef
Tyler O'Donnell <tylodonnell@gmail.com>
2016-10-29 20:46:59 -0400

    Clean up app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
  /**
   * @Frunt
   * @author Piccirilli Dorsey
-  *
+  * /
+
+ /**
   * First we will load all of this project's JavaScript dependencies which
   * include Vue and Vue Resource. This gives a great starting point for
   * building robust, powerful web applications using Vue and Frunt.
@@ -9,8 +11,13 @@
 
 import './bootstrap';
 
-const app = new Vue({
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
+const app = new Vue({
     el: '.o-app',
 
     data: {},
@@ -20,5 +27,4 @@ const app = new Vue({
     },
 
     methods: {}
-
 });
