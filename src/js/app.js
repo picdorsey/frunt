0adb8baaa2c93366deb13b7cc01b57f9697e6e3a
Tyler O'Donnell <tylodonnell@gmail.com>
2016-10-29 20:49:59 -0400

    More cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
- /**
-  * @Frunt
-  * @author Piccirilli Dorsey
-  * /
+/**
+ * Frunt
+ * @author Piccirilli Dorsey
+ */
 
- /**
-  * First we will load all of this project's JavaScript dependencies which
-  * include Vue and Vue Resource. This gives a great starting point for
-  * building robust, powerful web applications using Vue and Frunt.
-  */
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * include Vue and Vue Resource. This gives a great starting point for
+ * building robust, powerful web applications using Vue and Frunt.
+ */
 
 import './bootstrap';
 
