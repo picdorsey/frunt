07767e4fb87ede79918721c53ae756a37d346f87
Tyler O'Donnell <tyler@tylerodonnell.com>
2017-03-15 12:15:23 -0400

    Use webpack for dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,6 @@ let mix = require('laravel-mix');
 mix.setPublicPath('public');
 
 mix.js('src/js/app.js', 'public/assets/js/bundle.js')
-   .combine([], 'public/assets/js/vendor.js')
    .sass('src/scss/style.scss', 'public/assets/css')
-   .sass('src/scss/guide.scss', 'public/guide/assets/css')
+   // .sass('src/scss/guide.scss', 'public/guide/assets/css')
    .browserSync();
