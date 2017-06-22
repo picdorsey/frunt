17192578deaa553c57f05313e5bed0a86403d1a5
Tyler O'Donnell <tyler@tylerodonnell.com>
2017-06-22 11:06:05 -0400

    Upgrade to Laravel Mix 1.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,20 +15,22 @@ mix.webpackConfig({
     }
 });
 
+const browserSync = {
+    host: 'localhost',
+    port: 3000,
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/**/*.html',
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ],
+    server: {
+        baseDir: 'public',
+    },
+    proxy: null,
+};
+
 mix.js('src/js/app.js', 'public/js/bundle.js')
     .sass('src/scss/style.scss', 'public/css/style.css')
-    .browserSync({
-        host: 'localhost',
-        port: 3000,
-        files: [
-            'app/**/*.php',
-            'resources/views/**/*.php',
-            'public/**/*.html',
-            'public/js/**/*.js',
-            'public/css/**/*.css'
-        ],
-        server: {
-            baseDir: 'public',
-        },
-        proxy: null,
-    });
+    .browserSync(browserSync);
